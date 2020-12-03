a1f3e45ffc4edd65e7240af0eac601007e90b4bf
lenny <lennylee91@gmail.com>
2020-12-03 12:03:06 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,6 +1,6 @@
 export function manageFriends(state, action){
 
-    function reducer(state, action){      
+    function reducer(statestate = {count: 0}, action){      
         switch (action.type) {
           case 'ADD_FRIEND':
             return {count: state.count + 1}
